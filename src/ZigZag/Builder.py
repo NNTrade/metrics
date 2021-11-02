2ed92fe4abd8bf0993acef1f9a3c606821e055e3
InsonusK <Insonus.K@gmail.com>
2021-11-03 01:17:19 +0300

    Fix bug and change visualization

--- src/ZigZag/Builder.py
+++ src/ZigZag/Builder.py
@@ -15,18 +15,28 @@ class ZigZagBuilder:
         pass
 
     def build_flags(self, data_sr:pd.Series)->pd.Series:
+        data_idx_sr = data_sr.reset_index(drop=True)
+        return self.__build_flags__(data_idx_sr)
+
+    def __build_flags__(self, data_sr:pd.Series)->pd.Series:
         pivot = peak_valley_pivots(data_sr, self.up_thresh, self.down_thresh)
         return pd.Series(pivot,index=data_sr.index,name=self.flag_col_name)
     
-    def __build_values__(self, data_sr:pd.Series,flag_sr:pd.Series)->pd.Series:
+    def __build_values__(self, data_sr:pd.Series,flag_sr:pd.Series=None)->pd.Series:
+        if not (flag_sr is not None):
+            flag_sr = self.__build_flags__(data_sr)
+
         filtered_data = data_sr[flag_sr != 0]
         return pd.Series(np.interp(data_sr.index, filtered_data.index, filtered_data), index=data_sr.index, name=self.value_col_name)
 
     def build_values(self, data_sr:pd.Series)->pd.Series:
-        flag_sr = self.build_flags(data_sr)
-        return self.__build_values__(data_sr, flag_sr)
+        data_idx_sr = data_sr.reset_index(drop=True)        
+        return self.__build_values__(data_idx_sr)
 
-    def __build_nearest_ext__(self, data_sr:pd.Series, flag_sr:pd.Series)->pd.Series:
+    def __build_nearest_ext__(self, data_sr:pd.Series, flag_sr:pd.Series = None)->pd.Series:
+        if not (flag_sr is not None):
+            flag_sr = self.__build_flags__(data_sr)
+
         work_df = pd.concat([data_sr.rename("val"), flag_sr], axis=1).sort_index(ascending=False)
         class nearest_ext_worker:
             def __init__(self) -> None:
@@ -43,15 +53,16 @@ class ZigZagBuilder:
         return work_df.apply(lambda row: near_ext_wrk.do(row["val"], row[self.flag_col_name]),axis=1).sort_index(ascending=True).rename(self.nearest_col_name)
 
     def build_nearest_ext(self, data_sr:pd.Series)->pd.Series:
-        flag_sr = self.build_flags(data_sr)
-        return self.__build_nearest_ext__(data_sr,flag_sr)
+        data_idx_sr = data_sr.reset_index(drop=True)        
+        return self.__build_nearest_ext__(data_idx_sr)
 
     def __build_delta_to_near_ext__(self,data_sr:pd.Series,near_ext_sr:pd.Series)->pd.Series:
         return (near_ext_sr - data_sr).rename(self.delta_near_ext_name)
 
     def build_delta_to_near_ext(self,data_sr:pd.Series)->pd.Series:
-        near_ext_sr = self.build_nearest_ext(data_sr=data_sr)
-        return self.__build_delta_to_near_ext__(data_sr, near_ext_sr)
+        data_idx_sr = data_sr.reset_index(drop=True)
+        near_ext_sr = self.__build_nearest_ext__(data_sr=data_idx_sr)
+        return self.__build_delta_to_near_ext__(data_idx_sr, near_ext_sr)
 
     def __build_angle__(self, val_sr:pd.Series)->pd.Series:
         class worker:
@@ -70,16 +81,18 @@ class ZigZagBuilder:
         return val_sr.apply(wrk.next).shift(periods=-1).rename(self.angle_col_name)
 
     def build_angle(self,data_sr:pd.Series)->pd.Series:
-        val_sr = self.build_values(data_sr)
+        data_idx_sr = data_sr.reset_index(drop=True)
+        val_sr = self.__build_values__(data_idx_sr)
         return self.__build_angle__(val_sr)
 
 
     def build_all(self, data_sr:pd.Series)->pd.DataFrame:
-        flag_sr = self.build_flags(data_sr)
-        value_sr = self.__build_values__(data_sr, flag_sr)
+        data_idx_sr = data_sr.reset_index(drop=True)
+        flag_sr = self.__build_flags__(data_idx_sr)
+        value_sr = self.__build_values__(data_idx_sr, flag_sr)
         angle_sr = self.__build_angle__(value_sr)
-        nearest_ext_sr = self.__build_nearest_ext__(data_sr, flag_sr)
-        delta_near_ext_sr = self.__build_delta_to_near_ext__(data_sr,nearest_ext_sr)
+        nearest_ext_sr = self.__build_nearest_ext__(data_idx_sr, flag_sr)
+        delta_near_ext_sr = self.__build_delta_to_near_ext__(data_idx_sr,nearest_ext_sr)
         return pd.concat([flag_sr,value_sr,angle_sr, nearest_ext_sr,delta_near_ext_sr], axis=1)
 
 
