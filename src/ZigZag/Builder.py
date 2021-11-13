HYP-52 #time 3h Add function, next step is fix bug in calculation

src/ZigZag/Builder.py
@@ -2,6 +2,7 @@ import pandas as pd
 from zigzag import *
 import numpy as np
 from .Constant import FLAG_COL_NAME, NEXT_INDEX_COL_NAME, ROWS_TO_NEXT_EXT_COL_NAME, VALUE_COL_NAME,ANGLE_COL_NAME,NEAREST_EXT_COL_NAME,DELTA_NEAR_EXT_COL_NAME
+from .Tool.Rows_to_next_ext_builder import Rows_to_next_ext_builder
 
 class ZigZagBuilder:
     def __init__(self,up_thresh:float=0.02, down_thresh:float=-0.02) -> None:
@@ -98,30 +99,7 @@ class ZigZagBuilder:
 
     def rows_to_next_ext(self, data_sr:pd.Series)->pd.Series:
         flag_sr = self.build_flags(data_sr)
-        return self.__rows_to_next_ext__(data_sr, flag_sr)
-
-    def __rows_to_next_ext__(self, data_sr:pd.Series, flag_sr:pd.Series = None)->pd.Series:
-        class worker:
-            def __init__(self) -> None:
-                self.cur_counter = np.NAN
-                pass
-            def next(self, cur_val):
-                if np.isnan(cur_val):
-                    return np.NAN
-                else:
-                    if cur_val != 0:
-                        _ret = self.cur_counter + 1
-                        self.cur_counter = 0
-                        return _ret
-                    else:
-                        self.cur_counter = self.cur_counter + 1 
-                        return self.cur_counter
-
-        wrk = worker()
-        _ret_arr = []
-        for idx in reversed(flag_sr.index):
-            _ret_arr.append(wrk.next(flag_sr[idx]))
-        return pd.Series(reversed(_ret_arr), index=data_sr.index,name=ROWS_TO_NEXT_EXT_COL_NAME)
+        return Rows_to_next_ext_builder(flag_sr)
 
     def build_all(self, data_sr:pd.Series)->pd.DataFrame:
         data_idx_sr = data_sr.reset_index(drop=True)
@@ -132,7 +110,7 @@ class ZigZagBuilder:
         _ret[NEAREST_EXT_COL_NAME] = self.__build_nearest_ext__(data_sr, _ret[FLAG_COL_NAME])
         _ret[DELTA_NEAR_EXT_COL_NAME] = self.__build_delta_to_near_ext__(data_sr,_ret[NEAREST_EXT_COL_NAME])
         _ret[NEXT_INDEX_COL_NAME] = self.__build_next_index__(data_sr, _ret[FLAG_COL_NAME])
-        _ret[ROWS_TO_NEXT_EXT_COL_NAME] = self.__rows_to_next_ext__(data_sr, _ret[FLAG_COL_NAME])
+        _ret[ROWS_TO_NEXT_EXT_COL_NAME] = Rows_to_next_ext_builder(_ret[FLAG_COL_NAME])
         return _ret
 
     def __prepare_return_index(self, ret_df:pd.DataFrame, data_sr:pd.Series):

src/ZigZag/Configurator/IsCorrectFunctions.py
@@ -1,34 +1,42 @@
-from ..Constant import FLAG_COL_NAME, ANGLE_COL_NAME
+from ..Tool import Rows_to_next_ext_builder
+from ..Constant import FLAG_COL_NAME, ANGLE_COL_NAME, ROWS_TO_NEXT_EXT_COL_NAME
 import pandas as pd
 from typing import List
+import numpy as np
 
 class Check_machine:
     def __init__(self,check_func_list:List):
         self.check_func_list = check_func_list
         pass
 
-    def check_is_correct(self, zz_df:pd.DataFrame)->bool:
+    def is_correct(self, zz_df:pd.DataFrame)->bool:
         for func in self.check_func_list:
             if not func(zz_df):
                 return False
         return True
 
 
-def check_no_same_move_direction(zz_df:pd.DataFrame):
+def no_same_move_direction_is_correct(zz_df:pd.DataFrame):
     picks_zz = zz_df[zz_df[FLAG_COL_NAME]!=0]
     angle_check:pd.Series = picks_zz[FLAG_COL_NAME] * picks_zz[ANGLE_COL_NAME]     
     return not angle_check[angle_check>0].any()   
 
 class Check_len_size:
     
-    def __init__(self,min_len = 10) -> None:
+    def __init__(self,min_len = 10, max_len = np.NaN) -> None:
         self.min_len = min_len
+        self.max_len = max_len
         pass
 
-    def check(self,zz_df:pd.DataFrame)->bool:
+    def is_correct(self,zz_df:pd.DataFrame)->bool:
         data_idx_sr = zz_df.reset_index(drop=True)
         idx_df = pd.DataFrame(data_idx_sr[data_idx_sr[FLAG_COL_NAME]!=0].index,columns=["idx"])
         idx_df["next_idx"] = idx_df["idx"].shift(-1)
         idx_df["move"] = idx_df["next_idx"] - idx_df["idx"]
-        return not idx_df[idx_df["move"] < self.min_len]["move"].any()
+        is_correct = True
+        if not np.isnan(self.min_len):
+            is_correct = is_correct and not idx_df[idx_df["move"] < self.min_len]["move"].any()
+        if not np.isnan(self.max_len):
+            is_correct = is_correct and not idx_df[idx_df["move"] > self.max_len]["move"].any()
+        return is_correct
         

src/ZigZag/Configurator/ParameterConfigurator.py
@@ -3,28 +3,17 @@ import numpy as np
 import pandas as pd
 from typing import List,Tuple
 from ..Builder import ZigZagBuilder
-from ..ProfitCalculator import get_prod_of_profit
-from .CorrectChecking import Check_machine, check_no_same_move_direction, Check_len_size
+from .CompareFunctions import Prod_Max_func
+from .IsCorrectFunctions import Check_machine, no_same_move_direction_is_correct, Check_len_size
 import logging
 from ...misc.counter import Counter
 from ..Constant import FLAG_COL_NAME, ANGLE_COL_NAME
 
-
-def prod_max_func(zz_df: pd.DataFrame, max_val: None)->Tuple[bool,float]:
-    prod = get_prod_of_profit(zz_df)
-    if np.isnan(max_val):
-        return (True, prod)
-    if prod > max_val:
-        return (True, prod)
-    else:
-        return (False,)
-            
 def __base_check__():
     len_checker = Check_len_size(10)
-    return Check_machine([check_no_same_move_direction, len_checker.check]).check_is_correct
+    return Check_machine([no_same_move_direction_is_correct, len_checker.is_correct]).is_correct
 
-def Search_by_range(sr:pd.Series, up_thresh_list:List[float], down_thresh_list:List[float], compare_func = prod_max_func, check_func = __base_check__(), check_zz_df:bool = True)->Tuple[float,float]:
-    max_val = np.NAN
+def Search_by_range(sr:pd.Series, up_thresh_list:List[float], down_thresh_list:List[float], compare_func = Prod_Max_func().is_best, check_func = __base_check__(), check_zz_df:bool = True)->Tuple[float,float]:
     best_parameter = np.NAN
     logger = logging.getLogger("Search_by_range")
     counter = Counter(len(up_thresh_list)*len(down_thresh_list))
@@ -38,8 +27,6 @@ def Search_by_range(sr:pd.Series, up_thresh_list:List[float], down_thresh_list:L
             
             if check_zz_df and not check_func(zz_df):
                 continue
-            comp_res = compare_func(zz_df, max_val)
-            if comp_res[0]:
-                max_val = comp_res[1]
+            if compare_func(zz_df):
                 best_parameter = (up_thresh, down_thresh)    
     return best_parameter